bbbb7990443de66f4363230a140eeeeee7d2a3b2
Cody <codybarker.or@gmail.com>
2023-04-13 11:40:59 -0700

    s

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
-  # write code here
+
+  attr_accessor :l1, :l2, :l3
+  
+  def initialize(l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+  end
+
+  def kind
+    if l1 == l2 && l2 == l3 then :equilateral
+    elsif l1 == l2 || l1 == l3 || l2 == l3 then :isosceles
+    elsif l1 != l2 || l3 then :scalene
+    elsif l1 <= 0 || l2 <= 0 || l3 <= 0 then raise TriangleError
+    elsif l1 + l2 < l3 ||  l2 + l3 < l1 || l1 + l3 < l2 then raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
