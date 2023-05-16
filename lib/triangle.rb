adfab443b7a1299c50fc4fafd4a59d42e4b8719e
Cody <codybarker.or@gmail.com>
2023-05-16 11:13:32 -0700

    s

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,12 +12,20 @@ class Triangle
     if l1 == l2 && l2 == l3 then :equilateral
     elsif l1 == l2 || l1 == l3 || l2 == l3 then :isosceles
     elsif l1 != l2 || l3 then :scalene
-    elsif l1 <= 0 || l2 <= 0 || l3 <= 0 then raise TriangleError
+    elsif l1 <= 0 || l2 <= 0 || l3 <= 0 
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
     elsif l1 + l2 < l3 ||  l2 + l3 < l1 || l1 + l3 < l2 then raise TriangleError
     end
   end
 
   class TriangleError < StandardError
+    def message
+      "This is not a triangle"
+    end
   end
 
 end
