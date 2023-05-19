dfe3a7f489a4b6af7417bb2b158bb6975ba413c5
Cody <codybarker.or@gmail.com>
2023-05-19 12:12:58 -0700

    s

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,13 +9,21 @@ class Triangle
   end
 
   def kind
+    raise TriangleError unless sides_positive(l1, l2, l3) && inequality(l1, l2, l3)
     if l1 == l2 && l2 == l3 then :equilateral
-    elsif l1 == l2 || l1 == l3 || l2 == l3 then :isosceles
-    elsif l1 != l2 || l3 then :scalene
-    elsif l1 <= 0 || l2 <= 0 || l3 <= 0 then raise TriangleError
-    elsif l1 + l2 < l3 ||  l2 + l3 < l1 || l1 + l3 < l2 then raise TriangleError
+    elsif l1 == l2 || l2 == l3 || l1 == l3 then :isosceles
+    elsif l1 != l2 && l2 != l3 then :scalene
     end
   end
+
+  def sides_positive(l1, l2, l3)
+    l1 > 0 && l2 > 0 && l3 > 0 ? true : false
+  end
+
+  def inequality(l1, l2, l3)
+    l1 + l2 >= l3 && l2 + l3 >= l1 && l1 + l3 >= l2 ? true : false
+  end
+
 
   class TriangleError < StandardError
   end
